c7defb17a6e6ae254fe51b1f399dba4d7e770069
Nimi Williams <williamson.nimi@gmail.com>
2024-04-04 00:01:03 +0100

    Hotfix

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -25,5 +25,5 @@ class BasicCache(BaseCaching):
         """
         if key is not None:
             return (self.cache_data[key])
-        if key is None or key is not in self.cache_data:
+        if key is None or !(key in self.cache_data):
             return None
