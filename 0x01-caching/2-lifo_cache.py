495fccada6a26f030928f314e2face7962d53537
Nimi Williams <williamson.nimi@gmail.com>
2024-04-06 22:11:00 +0100

    Lifo algorithm code logic rearrangement

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -4,7 +4,7 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ The FIFO Cache Class"""
+    """ The LIFO Cache Class"""
     def __init__(self):
         """ This is the constructor"""
         super().__init__()
@@ -15,18 +15,18 @@ class LIFOCache(BaseCaching):
         @item: The item to be inserted
         Return: Returns nothing
         """
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key = list(self.cache_data.keys())[-1]
-            self.cache_data.popItem()
-            print("DISCARD: ".format(first_key))
         if key is not None and item is not None:
             self.cache_data[key] = item
+        if len(list(self.cache_data.keys())) > BaseCaching.MAX_ITEMS:
+            last_key = list(self.cache_data.keys())[-2]
+            self.cache_data.pop(last_key)
+            print("DISCARD: {}".format(last_key))
 
     def get(self, key):
         """ This function gets the item from a cache using the key
         @key: The key to search with
         Return: Returns the value linked to the key
         """
-        if key is not None:
-            return (self.cache_data[key])
-        return None
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return (self.cache_data[key])
