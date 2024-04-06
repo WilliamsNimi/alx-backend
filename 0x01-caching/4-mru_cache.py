33150b8ef34210111518ef7d2100e0893db0feca
Nimi Williams <williamson.nimi@gmail.com>
2024-04-06 22:25:18 +0100

    Fix mru algorithm code logic issues

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -5,7 +5,7 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ The FIFO Cache Class"""
+    """ The MRU Cache Class"""
     def __init__(self):
         """ This is the constructor"""
         super().__init__()
@@ -17,20 +17,21 @@ class MRUCache(BaseCaching):
         @item: The item to be inserted
         Return: Returns nothing
         """
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            mru_key = max(self.log, key=self.log.get)
-            self.cache_data.pop(mru_key)
-            print("DISCARD: ".format(mru_key))
         if key is not None and item is not None:
             self.cache_data[key] = item
             self.log[key] = datetime.now()
+            if len(list(self.cache_data.keys())) > BaseCaching.MAX_ITEMS:
+                mru_key = max(self.log, key=self.log.get)
+                self.cache_data.pop(mru_key)
+                self.log.pop(mru_ke)y
+                print("DISCARD: {}".format(mru_key))
 
     def get(self, key):
         """ This function gets the item from a cache using the key
         @key: The key to search with
         Return: Returns the value linked to the key
         """
-        if key is not None:
-            self.log[key] = datetime.now()
-            return (self.cache_data[key])
-        return None
+        if key is None or key not in self.cache_data.keys():
+            return None
+        self.log[key] = datetime.now()
+        return (self.cache_data[key])
